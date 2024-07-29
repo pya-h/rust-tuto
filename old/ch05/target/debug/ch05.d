43c2f700a3fe6597c279ab9da07f6acafe3d39ac
pya-h <p4ya.gcc@gmail.com>
2024-07-29 11:35:02 +0330

    reviewing chapter 4 after a while

---
+++ old/ch05/target/debug/ch05.d
@@ -0,0 +1,1 @@
+/home/fr3ek/pya.h/rust/tutorials/ch05/target/debug/ch05: /home/fr3ek/pya.h/rust/tutorials/ch05/src/main.rs
---
+++ old/ch05/target/debug/deps/ch05-aff688ef03c21d68.d
@@ -0,0 +1,5 @@
+/home/fr3ek/pya.h/rust/tutorials/ch05/target/debug/deps/ch05-aff688ef03c21d68: src/main.rs
+
+/home/fr3ek/pya.h/rust/tutorials/ch05/target/debug/deps/ch05-aff688ef03c21d68.d: src/main.rs
+
+src/main.rs:
