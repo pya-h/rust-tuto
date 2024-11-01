fea0e28eb98390484cd4283b5155e00528ba6808
pya-h <p4ya.gcc@gmail.com>
2024-11-01 22:08:47 +0330

    add complete tests for both double and float in c/c++

--- tryouts/1.2-1.0/main.c
+++ tryouts/1.2-1.0/main.c
@@ -2,6 +2,10 @@
 
 int main() {
 	double a = 1.2, b = 1.0, c = -1.0;
-	float u = 2.6f, v = 2.0f;
-	printf("%.16f\t%.16lf", a + b * c, u + c * v);
+	double u = 2.6, v = 2.0;
+	printf("By double: %.16lf\t%.16lf\n", a + b * c, u + c * v);
+
+	float af = 1.2f, bf = 1.0f, cf = -1.0f;
+	float uf = 2.6f, vf = 2.0f;
+	printf("\tBy float: %.16f\t%.16f", af + bf * cf, uf + cf * vf);
 }
--- tryouts/1.2-1.0/main.cpp
+++ tryouts/1.2-1.0/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 int main() {
-	float a = 2.6, b = 2.0f, c = -1.0;
-	double u = 1.2, v = 1.0;
-	std::cout << u + c * v << "\t";
-	std::cout << a + c * b;
+	double a = 1.2, b = 1.0, c = -1.0;
+	double u = 2.6, v = 2.0;
+	std::cout << "By double: "<< a + b * c << "\t" << u + c * v<< std::endl;
+
+	float af = 1.2f, bf = 1.0f, cf = -1.0f;
+	float uf = 2.6f, vf = 2.0f;
+	std::cout << "\tBy float: " << af + bf * cf << "\t" << uf + cf * vf;
 }
