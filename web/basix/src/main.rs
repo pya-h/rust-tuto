6958a3636f33abe20a5afccba6c99c2c7c18a838
pya-h <p4ya.gcc@gmail.com>
2025-11-30 04:28:09 +0330

    practice different types of get routes

--- web/basix/src/main.rs
+++ web/basix/src/main.rs
@@ -1,16 +1,37 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr};
 
-use axum::{ Router, response::{ Html, IntoResponse }, routing::get };
+use axum::{ Router, extract::{Path, Query}, response::{ Html, IntoResponse }, routing::get };
+use serde::Deserialize;
+
+#[allow(unused)]
+
+#[derive(Debug, Deserialize)]
+struct GreetingParam {
+    name: Option<String>,
+}
+
+
+
+fn get_routes() -> Router {
+    Router::new()
+        .route("/", get(|| async { Html("Root <strong>Route!?</strong>") }))
+        .route("/another", get(routes_another_handler))
+        .route("/greet", get(routes_greet_handler))
+        .route("/greet/:times", get(routes_greet_n_times_handler))
+}
+
 
 #[tokio::main]
 async fn main() {
-    let routes = Router::new()
-        .route(
-            "/",
-            get(|| async { Html("Root <strong>Route!?</strong>") })
-        )
-        .route("/another", get(routes_another_handler));
-
+    // let routes = Router::new()
+    //     .route(
+    //         "/",
+    //         get(|| async { Html("Root <strong>Route!?</strong>") })
+    //     )
+    //     .route("/another", get(routes_another_handler))
+    //     .route("/greet", get(routes_greet_handler))
+    //     .route("/greet/:times", get(routes_greet_n_times_handler));
+    let routes = Router::new().merge(get_routes());
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     println!("Listening on {}", addr);
@@ -19,5 +40,23 @@ async fn main() {
 }
 
 async fn routes_another_handler() -> impl IntoResponse {
+    println!("->> {:<12} Route called!", "/another");
     Html("<h1>Another 1</h1>")
+}
+
+async fn routes_greet_handler(Query(queries): Query<GreetingParam>) -> impl IntoResponse {
+    println!("->> {:<12} Route called with the query: {:?}", "/greet", queries);
+    let name = queries.name.as_deref().unwrap_or("Whatever!");
+
+    Html(format!("<h2>Hello <u>{name}</u></h2>"))
+}
+
+async fn routes_greet_n_times_handler(Path(times): Path<u64>, Query(queries): Query<GreetingParam>) -> impl IntoResponse {
+    println!("->> {:<12} Route called with the query: {:?}, params: {:?}", "/greet", queries, times);
+    let name = queries.name.as_deref().unwrap_or("Whatever");
+    let mut response = String::new();
+    for _ in 0..times {
+        response.push_str(format!("<h2>Hello <u>{name}</u></h2>").as_str());
+    }
+    Html(response)
 }
--- web/basix/tests/dev_mode.rs
+++ web/basix/tests/dev_mode.rs
@@ -16,3 +16,22 @@ async fn fetch_another() -> Result<()> {
 
     Ok(())
 }
+
+
+#[tokio::test]
+async fn greet_paya() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+
+    hc.do_get("/greet?name=paya").await?.print().await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn greet_paya_n_times() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+
+    hc.do_get("/greet/10?name=paya").await?.print().await?;
+
+    Ok(())
+}
